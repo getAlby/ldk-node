7f82ce5d1f771da03f02491aa45501812061043b
Roland Bewick <roland.bewick@gmail.com>
2025-08-08 13:33:37 +0700

    chore: bump vss retry policy attempts and timeout

--- src/io/vss_store.rs
+++ src/io/vss_store.rs
@@ -61,9 +61,18 @@ impl VssStore {
 			derive_data_encryption_and_obfuscation_keys(&vss_seed);
 		let key_obfuscator = KeyObfuscator::new(obfuscation_master_key);
 		let storable_builder = StorableBuilder::new(data_encryption_key, RandEntropySource);
-		let retry_policy = ExponentialBackoffRetryPolicy::new(Duration::from_millis(10))
-			.with_max_attempts(10)
-			.with_max_total_delay(Duration::from_secs(15))
+		let retry_policy: FilteredRetryPolicy<
+			JitteredRetryPolicy<
+				MaxTotalDelayRetryPolicy<
+					MaxAttemptsRetryPolicy<ExponentialBackoffRetryPolicy<VssError>>,
+				>,
+			>,
+			Box<dyn Fn(&VssError) -> bool + Send + Sync + 'static>,
+		> = ExponentialBackoffRetryPolicy::new(Duration::from_millis(10))
+			//.with_max_attempts(10)
+			.with_max_attempts(15) // Alby: account for unexpected networking errors
+			//.with_max_total_delay(Duration::from_secs(15))
+			.with_max_total_delay(Duration::from_secs(180)) // Alby: account for unexpected networking errors
 			.with_max_jitter(Duration::from_millis(10))
 			.skip_retry_on_error(Box::new(|e: &VssError| {
 				matches!(
